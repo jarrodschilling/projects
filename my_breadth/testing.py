0d2ac5933e699fa619744cc1cbf4f99ce5967882
jarrodschilling <129313146+jarrodschilling@users.noreply.github.com>
2023-08-26 13:15:26 +0000

    working on symbol check in testing.py

--- my_breadth/testing.py
+++ my_breadth/testing.py
@@ -1,9 +1,10 @@
 import sqlite3
 from functions import moving_avgs, login_required
 from tradingview_ta import TA_Handler, TradingView, Exchange, Interval, get_multiple_analysis
+from yahooquery import Ticker
 
 
-symbol1 = "XHR"
+symbol1 = "XAR"
 exchange1 = "AMEX"
 
 symbol2 = "JPM"
@@ -11,5 +12,27 @@ exchange2 = "NYSE"
 
 symbol3 = "XLF"
 exchange3 = "AMEX"
+
+symbols = [
+    ["XAR", "AMEX"],
+    ["JPM", "NYSE"],
+    ["XLF", "AMEX"],
+]
+
+#symbol1 = Ticker(symbol1)
+
+#print(symbol1.price)
+
+def symbol_check(symbol, exchange):
+    try:
+        print(moving_avgs(symbol, "america", exchange))
+    except Exception as e:
+        print(f"Symbol: {symbol} or Exchange: {exchange} not found")
+
+
+#for i in symbols:
+    #print(symbols[i][0], symbols[i][1])
+
+print(symbols[0][0], symbols[0][1])
 
 
