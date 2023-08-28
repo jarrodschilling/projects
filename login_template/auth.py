621d3166fd30686ae1a2fd2e2d8e7d3a3c099043
jarrodschilling <129313146+jarrodschilling@users.noreply.github.com>
2023-08-28 19:34:35 +0000

    1

--- login_template/auth.py
+++ login_template/auth.py
@@ -1,6 +1,12 @@
 from flask import Blueprint, render_template, url_for, request, redirect
+from werkzeug.security import generate_password_hash
+from .models import User
+from . import db
+import sqlite3
+
 
 auth = Blueprint("auth", __name__)
+
 
 @auth.route("/register")
 def register():
@@ -13,7 +19,16 @@ def signup_post():
     name = request.form.get("name")
     password = request.form.get("password")
 
-    print(email, name, password)
+    #print(email, name, password)
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        print("User already exists!")
+
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method="sha256"))
+    db.session.add(new_user)
+    db.session.commit()
 
     return redirect(url_for("auth.login"))
 
